3d571be247adcf84c935c4b1b4ab38a9e533a9f4
Paul D'Hubert <paul.dhubert@lifen.fr>
2024-06-27 09:34:16 +0200

    feat: download latest keymap from zed repo and merge user keymap

--- colemak_keybinding.rb
+++ colemak_keybinding.rb
@@ -20,7 +20,7 @@ def colemak_friendly(bindings, key_mappings)
   bindings.each do |key, value|
     # Split the key by space to handle combinations like 'ctrl-w k'
     parts = key.split(' ')
-    
+
     # Map each part of the combination using key_mappings
     new_parts = parts.map do |part|
       # Handle the case where '-' is a keybinding by itself
@@ -30,10 +30,10 @@ def colemak_friendly(bindings, key_mappings)
         part.split('-').map { |subpart| key_mappings.fetch(subpart, subpart) }.join('-')
       end
     end
-    
+
     # Join the parts back together to form the new key
     new_key = new_parts.join(' ')
-    
+
     # Recursively process nested hashes or arrays
     new_value = if value.is_a?(Hash)
                   colemak_friendly(value, key_mappings)
@@ -51,6 +51,7 @@ end
 
 # Read the JSON configuration from a file
 file_path = 'vim.json'
+system("curl -L https://raw.githubusercontent.com/zed-industries/zed/master/assets/keymaps/vim.json > #{file_path}")
 json_data = File.read(file_path)
 config = JSON.parse(json_data)
 
@@ -58,6 +59,16 @@ config = JSON.parse(json_data)
 config.each do |entry|
   entry['bindings'] = colemak_friendly(entry['bindings'], key_mappings)
 end
+
+# Apply the user key bindings from user.json
+user_file_path = 'user.json'
+if File.exist?(user_file_path)
+  user_json_data = File.read(user_file_path)
+  user_config = JSON.parse(user_json_data)
+
+  config.concat(user_config)
+end
+
 
 # Write the modified configuration back to a file or print it
 File.write('colemak_keybindings.json', JSON.pretty_generate(config))
